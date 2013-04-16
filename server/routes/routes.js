e3015d4958f56149aeeb9fa054090d2481203439
Matt Davis <mdavis@nextgen.com>
2013-04-16 17:46:55 -0500

    commented out a reference to a file on my machine

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,7 @@
 module.exports = function (app) {
 
     // user routes
-    var stock = require('../controllers/stock')
+    //var stock = require('../controllers/stock')
 
     app.get('/bands/:bandId', function(){return 'yo'});
 
